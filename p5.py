d714fdb471ff823c650edc81505a98a883ec21b1
Ryan <schroener1@gmail.com>
2016-09-26 20:01:24 -0700

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,7 +1,6 @@
 from itertools import count, takewhile
 
 def primes(n):
-    "Generate prime numbers up to n"
     seen = list()
     for i in xrange(2, n + 1):
         if all(map(lambda prime: i % prime, seen)):
@@ -12,7 +11,6 @@ def smallest(n):
     result = 1
     for prime in primes(n):
         bprime = max(takewhile(lambda x:x<=n, (prime ** c for c in count(1))))
-        # we could just take last instead of max()
         result *= bprime
     return result
 
